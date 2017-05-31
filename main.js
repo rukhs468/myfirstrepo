c725ca637f732800e8269c46203c6391295bbb01
Rukhsar Dungaria <rdungaria@yahoo.com>
2017-05-31 13:41:23 -0400

    updated

--- main.js
+++ main.js
@@ -12,17 +12,58 @@ console.log('hello');
   $('[data-spy="scroll"]').each(function() {
     var $spy = $(this).scrollspy('refresh')
   });
-  $('#contactp').hide();
+  $('.contactp').hide();
 
   var contact = false;
 
   $('#contactbutton').click(function(){
     contact = !contact
     if(contact){
-      $('#contactp').show();
+      $('.contactp').show();
     }else {
-      $('#contactp').hide();
+      $('.contactp').hide();
     }
   });
 
+  $('#traveldes').hide();
+  $('#examdes').hide();
+  var travel = false;
+
+  $('#travel').hover(function(){
+    travel = !travel
+    if(travel){
+      $('#traveldes').show();
+    }else {
+      $('#traveldes').hide();
+    }
+  });
+
+  var exam = false;
+  $('#examination').hover(function(){
+    exam = !exam
+    if(exam){
+      $('#examdes').show();
+    }else {
+      $('#examdes').hide();
+    }
+  });
+  $('#education').hide();
+  $('#interest').hide();
+
+  $('#back').click(function() {
+    $('#background').slideDown("slow");
+    $('#education').slideUp("slow");
+    $('#interest').slideUp("slow");
+  });
+
+  $('#ed').click(function() {
+    $('#education').slideDown("slow");
+    $('#background').slideUp("slow");
+    $('#interest').slideUp("slow");
+  });
+  $('#int').click(function() {
+    $('#interest').slideDown("slow");
+    $('#background').slideUp("slow");
+    $('#education').slideUp("slow");
+  });
 });
