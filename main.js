a395973b156f7ab26ca7ce6c8caa2ac3ca64c317
Rukhsar Dungaria <rdungaria@yahoo.com>
2017-06-02 11:21:06 -0400

    updated

--- main.js
+++ main.js
@@ -29,25 +29,83 @@ console.log('hello');
   $('#examdes').hide();
   var travel = false;
 
-  $('#travel').hover(function(){
+  $('#travel').click(function(){
     travel = !travel
     if(travel){
       $('#traveldes').show();
+      $('#examdes').hide();
     }else {
       $('#traveldes').hide();
     }
   });
 
   var exam = false;
-  $('#examination').hover(function(){
+  $('#examination').click(function(){
     exam = !exam
     if(exam){
       $('#examdes').show();
+      $('#traveldes').hide();
     }else {
       $('#examdes').hide();
     }
   });
-  $('#education').hide();
+  var background = false;
+
+  $('#back').click(function(){
+    background = !background
+    if(background){
+      $('#background').css({
+        'visibility':'visible',
+        'opacity':1,
+        'height':'400px'
+      });
+    }else {
+      $('#background').css({
+        'visibility':'hidden',
+        'opacity':0,
+        'height':'0px'
+      });
+    }
+  });
+
+  var education = false;
+
+  $('#ed').click(function(){
+    education = !education
+    if(education){
+      $('#education').css({
+        'visibility':'visible',
+        'opacity':1,
+        'height':'400px'
+      });
+    }else {
+      $('#education').css({
+        'visibility':'hidden',
+        'opacity':0,
+        'height':'0px'
+      });
+    }
+  });
+
+  var interest = false;
+
+  $('#int').click(function(){
+    interest = !interest
+    if(interest){
+      $('#interest').css({
+        'visibility':'visible',
+        'opacity':1,
+        'height':'400px'
+      });
+    }else {
+      $('#interest').css({
+        'visibility':'hidden',
+        'opacity':0,
+        'height':'0px'
+      });
+    }
+  });
+  /*$('#education').hide();
   $('#interest').hide();
 
   $('#back').click(function() {
@@ -65,5 +123,5 @@ console.log('hello');
     $('#interest').slideDown("slow");
     $('#background').slideUp("slow");
     $('#education').slideUp("slow");
-  });
+  });*/
 });
